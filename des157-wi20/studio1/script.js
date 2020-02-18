71ca15f75820afec2ebe052af82b2fda06a5aff1
Nehal Jain <nnjain@ucdavis.edu>
2020-02-17 23:43:36 -0800

    studio

--- des157-wi20/studio1/script.js
+++ des157-wi20/studio1/script.js
@@ -1,35 +1,28 @@
 (function(){
     "use strict";
 
-    const myform = document.querySelector("form");
+    document.getElementById("myform").addEventListener("submit", function(event){
     event.preventDefault();
-
-    //gets all data from form
-          const formData = document.querySelectorAll("input[type=text]");
-    
-    //create an array to hold values from form
-          const fill = [];
-    
-    //loops through the fields and adds the word to the array
-          for(var i=0; i<formData.length; i++){
-              fill.push(formData[i].value);
-          }
-    
-    //pass the array of words the user type into the function
-          makeMadLib(fill);
-      });
+    const formData = document.querySelectorAll("input[type=text]");
+    const fill = [];
+    for(var i=0; i<formData.length; i++){
+        fill.push(formData[i].value);
+    }
+  
+    makeMadLib(fill);
+    });
     
     //takes an array input, then constructs the madLib
-      function makeMadLib(thefiller){
-          const madlib = `My ${thefiller[0]}. When we first met, you looked so ${thefiller[1]} and ${thefiller[2]}. I couldn't believe my eyes. You smelled like a ${thefiller[3]}. You walked so ${thefiller[4]}. I couldn't wait to ${thefiller[5]} you.Hi ${yname}. I really like ${noun} too since it is ${adj1}. Please keep in mind that  that sugar and sweet things are harmful if not rationalized .While you enjjoy  ${adj1} ${adverb}, it is important to realise that sugar can cause harmful effects like tooth decay, obesity and anxiety.`;
-    
-    //get madlib container
-          const madlibCont = document.getElementById('madlib');
-    
-    //change the guts of the madlib container
+    function makeMadLib(fill){
+        const madlib = `Hi ${fill[0]}. I really like ${fill[1]} too since it is ${fill[2]}. Please keep in mind that  that sugar and sweet things are harmful if not rationalized .While you enjjoy  ${fil[3]} ${fill[4]}, it is important to realise that sugar can cause harmful effects like tooth decay, obesity and anxiety.`;
+        
+        //get madlib container
+        const madlibCont = document.getElementById('madlib');
+        
+        //change the guts of the madlib container
         madlibCont.innerHTML = `<p>${madlib}</p>`;
-    
-    //change the class to visible in order to see it
+        
+        //change the class to visible in order to see it
         madlibCont.setAttribute("class", "visible");
-      }
+        }
 }());
