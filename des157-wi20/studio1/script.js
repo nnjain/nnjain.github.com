9e3d89753066d025376d27ed32d7b5d4b3b81ee3
Nehal Jain <nnjain@ucdavis.edu>
2020-02-17 23:47:22 -0800

    research

--- des157-wi20/studio1/script.js
+++ des157-wi20/studio1/script.js
@@ -11,18 +11,14 @@
   
     makeMadLib(fill);
     });
-    
-    //takes an array input, then constructs the madLib
+
     function makeMadLib(fill){
         const madlib = `Hi ${fill[0]}. I really like ${fill[1]} too since it is ${fill[2]}. Please keep in mind that  that sugar and sweet things are harmful if not rationalized .While you enjjoy  ${fil[3]} ${fill[4]}, it is important to realise that sugar can cause harmful effects like tooth decay, obesity and anxiety.`;
-        
-        //get madlib container
+       
         const madlibCont = document.getElementById('madlib');
         
-        //change the guts of the madlib container
         madlibCont.innerHTML = `<p>${madlib}</p>`;
         
-        //change the class to visible in order to see it
         madlibCont.setAttribute("class", "visible");
         }
 }());
